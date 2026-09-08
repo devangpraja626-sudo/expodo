0c4d2b7a440e12d24d137cdf2808a993b54421c5
devangpraja626-sudo <devangpraja626@gmail.com>
2026-09-09 00:54:15 +0530

    Update profile.js

--- frontend/profile.js
+++ frontend/profile.js
@@ -10,7 +10,7 @@ const SUPABASE_URL =
   "https://inhxlwsjlddhnpalbocl.supabase.co";
 
 const SUPABASE_ANON_KEY =
-  "sb_publishable_EoecvlHpO_r1ZJzJdJWl5Q_VEgr0dOw";
+  "sb_publishable_g3o2x8l0tr3hHpqndi-wLg_zfYfFaXb";
 
 let supabaseClient = null;
 let profile = null;
